9b5172f01171f7fc3f27a080f5b85448ff63817f
TrentekaHarris <trentekaharris@gmail.com>
2022-11-08 13:33:19 -0500

    created task manager with form validation and reset

--- js/index.js
+++ js/index.js
@@ -1,1 +1,47 @@
+//Test Task Manager
 
+class Task extends TaskManager
+{constructor(tasks)
+  {super(tasks)}
+  
+}
+
+const taskNew = new Task();
+
+
+
+//Variable for form
+const form  = document.querySelector('#newTaskForm');
+
+// Get data from form
+function getData(form) {
+  const formData = new FormData(form);
+
+  for (const task of formData.entries()) {
+    taskNew.addTask(document.getElementById("newTaskNameInput").value,document.getElementById("newTaskDescription").value,document.getElementById("newTaskAssignedTo").value,document.getElementById("newTaskDueDate").value);
+  }
+//code to get status place after Due date
+ //, document.getElementById("newTaskStatus").value 
+}
+
+//function to check values from form
+async function hasValue(input, message) {
+  if (input.value.trim() === "") {
+    return console.log('error');
+    
+  }
+}
+
+//Event for submit
+document.getElementById("newTaskForm").addEventListener("submit", function (event) {
+  //Handle invalid data & prevent default
+  event.preventDefault();
+  let nameValid = hasValue(form.elements['newTaskNameInput']);
+  //form validation is successful
+  getData(event.target);
+
+  //Reset form
+form.reset();
+});
+
+console.log(taskNew.tasks)
--- js/taskManager.js
+++ js/taskManager.js
@@ -1,7 +1,38 @@
 //Task Manager Class
-
-class TaskName {
-    constructor() {
-        this.task = [];
+class TaskManager {
+   
+   constructor(tasks = [], currentId) {
+        this._tasks = tasks;
+        this._currentId = 0;
+            
+   }
+    //Add task to list
+    get tasks() {
+        return this._tasks;
     }
+    /*increaseTask(newTask){
+        return this._tasks.push(newTask);
+    }
+    
+    //Increase Id
+    currentId () {
+        console.log(`TO DD: ${this.tasks.length}`)
+    }*/
+
+    //Add Task Method
+    addTask(newTaskNameInput, newTaskDescription, newTaskAssignedTo, newTaskDueDate, status) {
+        const task = {
+            id: `TO DD: ${this.tasks.length+1}`,
+            newTaskNameInput: newTaskNameInput,
+            newTaskDescription: newTaskDescription,
+            newTaskAssignedTo: newTaskAssignedTo,
+            newTaskDueDate: newTaskDueDate,
+            status: status
+        };
+            return this._tasks.push(task) 
+    }
+    
 }
+
+
+  
