b39ce6d0e40786ea43be03f467556baa0923b38e
ZackM202 <mr.zxmiles@gmail.com>
2022-11-12 07:58:01 -0500

    task 4

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+//code that post new task form
+
+function formPopUp() {
+  var newTaskForm = document.getElementById("task"),
+      displayValue = "";
+      if (newTaskForm.style.display === "") {
+        newTaskForm.style.display = "none";
+      }
+
+  newTaskForm.style.display = displayValue;
+}
+
+
+
 
